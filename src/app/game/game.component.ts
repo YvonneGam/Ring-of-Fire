599ded709ba3ecd50a0110b45fbaddf4f417f880
unknown <yvonnegamboeck@web.de>
2021-04-10 15:45:06 +0200

    add players

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 @Component({
   selector: 'app-game',
@@ -11,7 +13,7 @@ export class GameComponent implements OnInit {
   currentCard: string | undefined = ''; //shows the name of the current card from the stack (lying on top)
   game!: Game;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.newGame();
@@ -37,4 +39,13 @@ export class GameComponent implements OnInit {
     }, 1200); //only allowed to click on the stack every 1500ms
   }
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name:string) => {
+      this.game.players.push(name);
+    });
+  }
+
 }
+
