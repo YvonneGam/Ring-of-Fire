d9420b421c9e8232ee715d745306ee649a28b061
unknown <yvonnegamboeck@web.de>
2021-04-16 17:15:17 +0200

    fix url bug & responsive design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { GameInfoComponent } from './game-info/game-info.component';
 import {MatCardModule} from '@angular/material/card';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
+import { PlayerMobileComponent } from './player-mobile/player-mobile.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { environment } from 'src/environments/environment';
     GameComponent,
     PlayerComponent,
     DialogAddPlayerComponent,
-    GameInfoComponent
+    GameInfoComponent,
+    PlayerMobileComponent
   ],
   imports: [
     BrowserModule,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA4DlKMF-aFiRxhQd-09DHIXzLSaMREplE",
+    authDomain: "yvonnefirebase.firebaseapp.com",
+    projectId: "yvonnefirebase",
+    storageBucket: "yvonnefirebase.appspot.com",
+    messagingSenderId: "915333230339",
+    appId: "1:915333230339:web:f92f31e5f31cc141e8c336"
+  }
 };
